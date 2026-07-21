f0f5aa2b9a3dd6e4060cbddba655e9714b056d9c
jineegood <fiostazi@gmail.com>
2026-07-22 01:07:54 +0900

    Update cloud-config.js

--- src/cloud-config.js
+++ src/cloud-config.js
@@ -2,6 +2,6 @@
    (docs/Supabase_설정_가이드.md 의 3단계에서 복사해 붙여넣으세요)
    값이 비어 있으면 기존처럼 이 컴퓨터(localStorage)에만 저장됩니다. */
 window.YUKSAM_CLOUD = {
-  url: 'https://eabxfedywcxbnfyyufcs.supabase.co/rest/v1/',      // 
+  url: 'https://eabxfedywcxbnfyyufcs.supabase.co',      // 
   anonKey: 'sb_publishable_ouCM28TznJ203HdBOTW9BQ__liCiBJ-',  // 
 };
